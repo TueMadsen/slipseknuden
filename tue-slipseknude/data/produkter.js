09ecc0ad9f02506717f035c8b2b7d0a6b9fbdb66
TueMadsen <60389@rts-365.dk>
2018-02-07 14:26:47 +0100

    Add files via upload

---
+++ tue-slipseknude/data/produkter.js
@@ -0,0 +1,49 @@
+module.exports = [{
+        'varenr': '1',
+        'producent': 'Shoon Slips',
+        'navn': 'Klassisk Brun',
+        'tekst': 'Et klassisk slips til den brune habit',
+        'pris': '900,-',
+        'billede': '/img/1_klassisk_brun.jpg'
+    },
+    {
+        'varenr': '2',
+        'producent': 'Shoon Slips',
+        'navn': 'Stribet Stil, Sort stribet',
+        'tekst': 'Et rigtigt direktørslips',
+        'pris': '650,-',
+        'billede': '/img/2_stribet_stil.jpg'
+    },
+    {
+        'varenr': '3',
+        'producent': 'Korea Special',
+        'navn': 'Brudgommens favorit',
+        'tekst': 'Skal du giftes? så skal det være det her!',
+        'pris': '350,-',
+        'billede': '/img/3_brudgommens_favorit.jpg'
+    },
+    {
+        'varenr': '4',
+        'producent': 'US Imports',
+        'navn': 'Sølvpil',
+        'tekst': 'Er du til fest og farver så vælg dette fantastiske slips.',
+        'pris': '600,-',
+        'billede': '/img/4_soelvpil.jpg'
+    },
+    {
+        'varenr': '5',
+        'producent': 'US Imports',
+        'navn': 'Rød F11',
+        'tekst': 'Slipset til den store charmør :-)',
+        'pris': '700,-',
+        'billede': '/img/5_roed_f11.jpg'
+    },
+    {
+        'varenr': '6',
+        'producent': 'Sleek Fabrics UK',
+        'navn': 'Blå kant',
+        'tekst': 'Køb vores blå slips til barnedåben!',
+        'pris': '560,-',
+        'billede': '/img/6_blaa_kant.jpg'
+    }
+];
